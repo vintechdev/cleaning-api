0489419d755a529fcd0756fdabd37055fdb2183b
Shraddha <shraddha.vaghani@gmail.com>
2021-01-11 19:27:36 +0530

    resolve conflicts

--- routes/api.php
+++ routes/api.php
@@ -84,17 +84,13 @@ Route::middleware(['auth:api', 'role:admin'])->namespace('Backend\API')->prefix(
 Route::post('getdashboard', 'HomeController@dashboard')->name('api.home.getdashboard')->middleware(['auth:api'])->middleware(['scope:customer']);
 
 Route::middleware(['auth:api','scope:customer,provider'])->namespace('Backend\API')->prefix('v1')->group(function () {
+    
     Route::patch('bookings/{booking}', 'BookingController@updateBooking')->name('update_booking')->middleware(['can:update,booking']);
     Route::patch('bookings/{booking}/dates/{recurring_date}', 'BookingController@updateRecurredBooking')->name('update_recurred_booking')->middleware(['can:update,booking']);
-    Route::get('bookings/{booking}', 'BookingController@getbookingdetails')->name('getbookingdetails');
+    Route::get('/bookings/{booking}', 'BookingController@getbookingdetails')->name('getbookingdetails');
     Route::get('bookings/{booking}/dates/{recurring_date}', 'BookingController@getbookingdetails')->name('getrecurredbookingdetails');
-<<<<<<< HEAD
-    Route::get('/bookings', 'BookingJobsController@listAllJobs');//->middleware('scope:customer');
-    Route::get('/allstatus', 'BookingController@listAllStatus');//->middleware('scope:customer,provider');
-=======
     Route::get('/bookings', 'BookingJobsController@listAllJobs');
-    Route::get('/allstatus', 'BookingController@listAllStatus')->middleware('scope:customer');
->>>>>>> dc6e82310c8f82471bfbee9180b3111eee8197e7
+    Route::get('/allstatus', 'BookingController@listAllStatus');
 });
 
 Route::middleware(['auth:api', 'role:customer'])->namespace('Backend\API')->prefix('v1/customer')->group(function () {

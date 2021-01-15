ff4012317d12362e286f49051be72c6b6f261456
Shruthi Rohith <rohith1505@gmail.com>
2021-01-15 19:03:13 +1000

    Pending list to not show bookings for rejected providers

--- app/Services/Bookings/PendingBookingList.php
+++ app/Services/Bookings/PendingBookingList.php
@@ -2,8 +2,10 @@
 
 namespace App\Services\Bookings;
 
+use App\Bookingrequestprovider;
 use App\Bookingstatus;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Class PendingBookingList
@@ -18,7 +20,8 @@ class PendingBookingList extends AbstractBookingList
     {
         $bookingQuery = $this->getBookingQuery()
             ->where('booking_status_id', Bookingstatus::BOOKING_STATUS_PENDING)
-            ->where('parent_booking_id', null);
+            ->where('parent_booking_id', null)
+            ->where('booking_request_providers.id', '<>', null);
 
         $from = $this->getFrom();
         $to = $this->getTo();
@@ -29,5 +32,14 @@ class PendingBookingList extends AbstractBookingList
         }
 
         return $bookingQuery->orderBy('booking_date', 'desc')->get('bookings.*');
+    }
+
+    /**
+     * @return Builder
+     */
+    protected function getProviderBookingQuery(): Builder
+    {
+        return parent::getProviderBookingQuery()
+            ->where('booking_request_providers.status', '<>', Bookingrequestprovider::STATUS_REJECTED);
     }
 }

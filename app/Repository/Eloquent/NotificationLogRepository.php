6e84ce78d8ddc82f8092e95026bb8d7f0e205594
Jigesh Parmar <jignesh.parmar126@gmail.com>
2021-02-17 09:37:53 +0530

    Add fromDate parameter or by default get count and list of undread notifcation of last 7 days

--- app/Repository/Eloquent/NotificationLogRepository.php
+++ app/Repository/Eloquent/NotificationLogRepository.php
@@ -6,6 +6,7 @@ namespace App\Repository\Eloquent;
 
 use App\NotificationLog;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -30,18 +31,29 @@ class NotificationLogRepository extends AbstractBaseRepository
     }
 
     private function countUserNotification(Request $request): int {
+        if (!$fromDate = $request->get('fromDate')) {
+            $fromDate = Carbon::now()->subDays('7')
+                ->format('Y-m-d 00:00:00');
+        }
+
         $count = NotificationLog::query()
             ->join('push_notification_logs',
                 'notification_logs.id','=','push_notification_logs.notification_log_id')
             ->where('push_notification_logs.status','unread')
             ->where('notification_logs.user_id', Auth::user()->id)
             ->where('push_notification_logs.user_type', $request->get('userType', 'user'))
+            ->where('push_notification_logs.updated_at', '>',  $fromDate)
             ->count();
 
         return intval($count);
     }
 
     private function getUserPushNotificationList(Request $request, string $status): array {
+        if (!$fromDate = $request->get('fromDate')) {
+            $fromDate = Carbon::now()->subDays('7')
+                ->format('Y-m-d 00:00:00');
+        }
+
         return NotificationLog::query()
         ->join('push_notification_logs',
             'notification_logs.id','=','push_notification_logs.notification_log_id')
@@ -50,6 +62,7 @@ class NotificationLogRepository extends AbstractBaseRepository
         ->orderBy('push_notification_logs.created_at', 'DESC')
         ->where('push_notification_logs.status', $status)
         ->where('push_notification_logs.user_type', $request->get('userType', 'user'))
+        ->where('push_notification_logs.updated_at', '>', $fromDate)
         ->get()
         ->toArray();
     }

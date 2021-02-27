20c040ba858e445b2a2fb7a86b432ceeae917230
Jigesh Parmar <jignesh.parmar126@gmail.com>
2021-02-27 17:16:08 +0530

    Add notification setting description and const to use whie storing notification in notification log table

--- app/Notification.php
+++ app/Notification.php
@@ -17,7 +17,28 @@ class Notification extends Model
     use SoftDeletes;
     protected $fillable = ['id'];
 
+    /**
+     * allow_* = 0 -> it is compulsory for user
+     * allow_* = 1 -> user will have choice to enable or disable
+     * allow_* = 2  -> will not be enabled for user (and will not appear in setting list)
+     **/
 
+    /**
+     * default_* = 0 -> default will be un checked
+     * default_* = 1 -> default will be checked
+     **/
+
+    /**
+     * display_provider = 1 -> notification type will be available on provider screen
+     * display_user = 1 -> notification type will be available on user screen
+     **/
+
+    const TRANSACTIONAL=  'transactional';
+    const BOOKING_UPDATES=  'booking_updates';
+    const REQUEST_TO_PROVIDER=  'new_booking_request_provider';
+    const TASK_REMINDER = 'task_reminder';
+    const HELP = "help_information";
+    const UPDATES = "updates_and_news_letters";
 
     public function userNotifications(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
